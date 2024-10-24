bf826e7ad4e2dad4015d883612f890d1b5a99b5c
Calciumcarbonate321 <visnuram2004@gmail.com>
2024-10-24 19:10:18 +0000

    chore: update it to have current endpoint for ml model

--- src/app/api/prediction/route.ts
+++ src/app/api/prediction/route.ts
@@ -1,48 +1,46 @@
 // app/api/prediction/[id]/route.ts
-import { NextResponse } from 'next/server'
+import { type NextRequest, NextResponse } from 'next/server'
 
-// interface SensorData {
-//   temperature: number
-//   humidity: number
-//   pressure: number
-// }
+interface SensorData {
+  temperature: number
+  humidity: number
+  pressure: number
+}
 
-// interface PredictionResponse {
-//   prediction: string
-// }
+interface PredictionResponse {
+  prediction: string
+}
 
-export async function POST() {
+export async function POST(request: NextRequest) {
   try {
-    //const sensorData: SensorData = await request.json()
+    const sensorData: SensorData = await request.json()
 
-    // if (!sensorData.temperature || !sensorData.humidity || !sensorData.pressure) {
-    //   return NextResponse.json(
-    //     { error: 'Missing required sensor data' },
-    //     { status: 400 }
-    //   )
-    // }
+    if (!sensorData.temperature || !sensorData.humidity || !sensorData.pressure) {
+      return NextResponse.json(
+        { error: 'Missing required sensor data' },
+        { status: 400 }
+      )
+    }
 
-    // const response = await fetch(`${process.env.PREDICTION_API_URL}/predict`, {
-    //   method: 'POST',
-    //   headers: {
-    //     'Authorization': `Bearer ${process.env.API_KEY}`,
-    //     'Content-Type': 'application/json'
-    //   },
-    //   body: JSON.stringify({
-    //     temperature: sensorData.temperature,
-    //     humidity: sensorData.humidity,
-    //     pressure: sensorData.pressure
-    //   })
-    // })
+    const response = await fetch(`${process.env.PREDICTION_API_URL}/predict`, {
+      method: 'POST',
+      headers: {
+        'Authorization': `Bearer ${process.env.API_KEY}`,
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        temperature: sensorData.temperature,
+        humidity: sensorData.humidity,
+        pressure: sensorData.pressure
+      })
+    })
 
-    // if (!response.ok) {
-    //   throw new Error(`External API responded with status: ${response.status}`)
-    // }
+    if (!response.ok) {
+      throw new Error(`External API responded with status: ${response.status}`)
+    }
 
-    // const predictionData: PredictionResponse = await response.json()
-
-    // return NextResponse.json({ prediction: predictionData.prediction })
-    return NextResponse.json({ prediction: "sunny" })
+    const predictionData: PredictionResponse = await response.json()
+    return NextResponse.json({ prediction: predictionData })
 
   } catch (error) {
     console.error('Error getting weather prediction:', error)
